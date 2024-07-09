627704e74abfcd70cb4953319cc0b24e94f8d254
Philip <fabianekphilip@gmail.com>
2024-07-09 20:30:44 +0200

    Add 'frontend' to template directories

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = "app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "frontend")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
